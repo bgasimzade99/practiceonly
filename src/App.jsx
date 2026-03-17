36489aaf01e892cc227f43240ff5ae8e493566ce
Coder999 <110833230+bgasimzade99@users.noreply.github.com>
2026-03-17 22:38:53 +0200

    about updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"; /// main seife bu da import
 import Contact from "./pages/Contact"; // contact pages actiq ona da import 
 import Products from "./pages/Product"; //product pages actig onu import
+import About from "./pages/About"; 
 import Navbar from "./components/Navbar"; //navbari burdan import
 import "./App.css"; //bu da esas her sikime olan css di
+
 function App() {
   return (
     <div className="app-layout">
@@ -14,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} /> 
           </Routes>
         </div>
       </main>
---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+import "../css/About.css";
+
+function About(){ 
+    return(
+        <div className="about-page-text"> 
+            <div className="about-page-content">
+                <h1>About us</h1>
+                <p>Hello my dear banans</p>
+    
+            </div>
+            
+            <div className="about-card"> 
+                <div className="about-card-content"> 
+                    <h1>Banana history</h1>
+                <p>
+                    There were 3 bananas were living in the forest, and one day <br/> One of them 
+                 Decided to move to city and start to code.<br/> And Here we are that last one banana
+                </p>
+                    </div>
+
+            </div>
+        </div>
+    ); 
+} 
+
+export default About;
